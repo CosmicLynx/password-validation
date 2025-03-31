f608dcde2323b01e6d8964c64ffd68e81c46b24c
Lynx <lynx@wiltner.net>
2025-03-31 13:58:45 +0200

    add list of common passwords; add method to check for common password

--- src/main/java/org/password/Main.java
+++ src/main/java/org/password/Main.java
@@ -1,21 +1,28 @@
 package org.password;
 
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scn = new Scanner(System.in);
         System.out.println("Please enter a password");
         String input = scn.nextLine();
 
-        if (!isPasswordAtLeast8CharactersLong(input))
-            System.out.println("Password must be at least 8 characters long.");
-        if (!passwordContainsNumbers(input))
-            System.out.println("Password must contain at least one number.");
-        if (!passwordContainsUppercaseLetter(input))
-            System.out.println("Password must contain at least one uppercase letter.");
-        if (!passwordContainsLowercaseLetter(input))
-            System.out.println("Password must contain at least one lowercase letter.");
+        if (!isPasswordAtLeast8CharactersLong(input) || !passwordContainsNumbers(input)
+                || !passwordContainsUppercaseLetter(input)
+                || !passwordContainsLowercaseLetter(input)) {
+            if (!isPasswordAtLeast8CharactersLong(input))
+                System.out.println("Password must be at least 8 characters long.");
+            if (!passwordContainsNumbers(input))
+                System.out.println("Password must contain at least one number.");
+            if (!passwordContainsUppercaseLetter(input))
+                System.out.println("Password must contain at least one uppercase letter.");
+            if (!passwordContainsLowercaseLetter(input))
+                System.out.println("Password must contain at least one lowercase letter.");
+        } else if (!passwordIsNotCommon(input))
+            System.out.println("Password is too common. Create a stronger password.");
 
         scn.close();
 
@@ -38,4 +45,9 @@ public class Main {
     public static boolean passwordContainsLowercaseLetter(String password) {
         return password.matches(".*[a-z].*");
     }
+
+    public static boolean passwordIsNotCommon(String password) {
+        return !Arrays.stream(CommonPasswords.mostCommonPasswords).anyMatch(password::equals);
+    }
+
 }

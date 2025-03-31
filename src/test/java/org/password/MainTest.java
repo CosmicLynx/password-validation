4407ecaaceef99b628f1abde52589541909420e7
Lynx <lynx@wiltner.net>
2025-03-31 10:48:43 +0200

    add tests for number validation

--- src/test/java/org/password/MainTest.java
+++ src/test/java/org/password/MainTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import org.junit.jupiter.api.Test;
 
 public class MainTest {
+
+    // Password Length
     @Test
     void test_isPasswordAtLeast8CharactersLong_trueIf10Chars() {
         assertTrue(Main.isPasswordAtLeast8CharactersLong("tencharspw"));
@@ -18,5 +20,16 @@ public class MainTest {
     @Test
     void test_isPasswordAtLeast8CharactersLong_trueIf8Chars() {
         assertTrue(Main.isPasswordAtLeast8CharactersLong("password"));
+    }
+
+    // Password contains numbers
+    @Test
+    void test_passwordContainsNumbers_trueIfContainsNumbers() {
+        assertTrue(Main.passwordContainsNumbers("password2"));
+    }
+
+    @Test
+    void test_passwordContainsNumbers_falseIfContainsNoNumbers() {
+        assertFalse(Main.passwordContainsNumbers("password"));
     }
 }
